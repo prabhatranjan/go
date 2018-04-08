93054e5af1c0075c07264473d7c32291a0681f6c
prabhatranjan <prabhatster@gmail.com>
2018-04-08 17:23:34 +0800

    execercise-slices

---
+++ excercise-slices/excercise-slices.go
@@ -0,0 +1,22 @@
+/*
+Implement Pic. It should return a slice of length dy, each element of which is a slice of dx 8-bit unsigned integers.
+When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
+*/
+
+package main
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+  s := make([][]uint8,dy)
+  for i:=range s{
+    s[i] = make([]uint8,dx)
+    for j:=range s[i]{
+      s[i][j]=uint8(i*j)
+    }
+  }
+  return s
+}
+
+func main() {
+	pic.Show(Pic)
+}
